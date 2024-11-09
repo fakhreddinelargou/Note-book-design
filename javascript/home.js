cdc2dc039789b951edeadc82701d3ec8988e0ddd
fakhreddine <largofakhrddin@gmail.com>
2024-11-09 11:55:30 +0100

    start project

---
+++ javascript/home.js
@@ -0,0 +1,63 @@
+const flow = document.querySelector('.flow')
+const singUp = document.querySelector('.singUp')
+const gmail = document.querySelector('.gmail')
+const password = document.querySelector('.pass')
+const problem = document.querySelector('.problem')
+problem.classList.add('hidden')
+singUp.classList.add('hidden')
+flow.classList.add('hidden')
+const creatD = document.querySelector('.text-creat-note')
+
+function create(){
+
+    creatD.classList.add('hidden')
+
+}
+// function singBtn() {
+//     // Assuming `gmail` is the input element (not the value itself)
+//     var emailInput = document.querySelector('.gmail'); // Get the input element
+//     var emailValue = emailInput.value; // Get the value of the input
+//     var passWord = document.querySelector('.pass')
+//     var passValue = passWord.value
+
+//     // Check if the email value matches
+//     if (emailValue === 'largofakhrddin@gmail.com' && passValue === '123456789') {
+//         flow.classList.add('hidden')
+//         singUp.classList.add('hidden')
+//     } else {
+//         problem.classList.remove('hidden')
+//     }
+    
+   
+    
+
+// }
+
+
+
+
+// notess
+
+
+const note1 =document.querySelector('.note1')
+const note2 =document.querySelector('.note2')
+const note3 =document.querySelector('.note3')
+const note4 =document.querySelector('.note4')
+const note5 =document.querySelector('.note5')
+const note6 =document.querySelector('.note6')
+const note7 =document.querySelector('.note7')
+const note8 =document.querySelector('.note8')
+const note9 =document.querySelector('.note9')
+const note10 =document.querySelector('.note10')
+
+// add hidden for notess
+note1.classList.add('hidden')
+note2.classList.add('hidden')
+note3.classList.add('hidden')
+note4.classList.add('hidden')
+note5.classList.add('hidden')
+note6.classList.add('hidden')
+note7.classList.add('hidden')
+note8.classList.add('hidden')
+note9.classList.add('hidden')
+note10.classList.add('hidden')
