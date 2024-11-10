c3c4b7f7ab6f8d47b068c4cca3810670c24dc391
fakhreddine <largofakhrddin@gmail.com>
2024-11-10 23:45:27 +0100

    modifed content

--- home.js
+++ home.js
@@ -3,24 +3,24 @@ const singUp = document.querySelector('.singUp')
 const gmail = document.querySelector('.gmail')
 const password = document.querySelector('.pass')
 const problem = document.querySelector('.problem')
-problem.classList.add('hidden')
-singUp.classList.add('hidden')
-flow.classList.add('hidden')
+
+
 const creatD = document.querySelector('.text-creat-note')
  // 
 function create(){
 
   creatD.classList.add('hidden')
-   notess.classList.remove('hidden')
+  flow.classList.remove('hidden')
+   allBox.classList.remove('hidden')
 }
+problem.classList.add('hidden')
 
-/*
  function singBtn() {
      // Assuming `gmail` is the input element (not the value itself)
      var emailInput = document.querySelector('.gmail'); // Get the input element     var emailValue = emailInput.value; // Get the value of the input
      var passWord = document.querySelector('.pass')
      var passValue = passWord.value
-
+      var emailValue = emailInput.value
      // Check if the email value matches
      if (emailValue === 'largofakhrddin@gmail.com' && passValue === '123456789') {
          flow.classList.add('hidden')
@@ -33,7 +33,7 @@ function create(){
     
 
 }
-*/
+
 
 
 
@@ -53,7 +53,7 @@ const note10 =document.querySelector('.note10')
 
 // add hidden for notess
 
-/*note1.classList.add('hidden')*/
+/*note1.classList.add('hidden')
 note2.classList.add('hidden')
 note3.classList.add('hidden')
 note4.classList.add('hidden')
@@ -63,18 +63,64 @@ note7.classList.add('hidden')
 note8.classList.add('hidden')
 note9.classList.add('hidden')
 note10.classList.add('hidden')
-
+*/
 
 
 
 
 // note 1
+const noteBox1 = document.querySelector('.note-box1')
+const infoBox = document.querySelector('.info-box')
+const readingBox = document.querySelector('.reading-box')
+const widthinfo = document.querySelector('.width-for-info-box')
+const widthreading = document.querySelector('.width-for-reading-box')
 
-const notess = document.querySelector('.notess')
-const remove = document.querySelector('.remove')
+noteBox1.classList.add('hidden')
+
+
+
 
 function btnOpen(){
-notess.classList.add('hidden')
-remove.classList.remove('hidden')
+    readingBox.classList.remove('hidden')
+readingBox.classList.add('width-for-reading-box')
+noteBox1.classList.add('width-for-reading-box')
+infoBox.classList.add('hidden')
+
 
 }
+function goback(){
+    noteBox1.classList.add('width-for-info-box')
+infoBox.classList.add('width-for-info-box')
+    readingBox.classList.add('hidden')
+    infoBox.classList.remove('hidden')
+    noteBox1.classList.remove('width-for-reading-box')
+
+}
+
+
+
+
+
+// inputs
+
+
+
+const title = document.querySelector('.title-for-head')
+const allBox = document.querySelector('.all-box')
+const inputName = document.getElementById('input-for-name')
+
+
+
+allBox.classList.add('hidden')
+
+function addtitle(){
+
+let namas = inputName.value
+title.textContent = namas
+flow.classList.add('hidden')
+allBox.classList.add('hidden')
+noteBox1.classList.remove('hidden')
+
+
+}
+
